2eed79400899ad811e4d25de9216b628b291bcc5
Sajal Swapnil <sajalvictorious83@gmail.com>
2022-01-24 19:27:36 +0530

    TwoMultiPlayerGame Java Code

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Player1 player = new Player1("Sajal","akm",100);
+//        System.out.println(player.getName());
+//        System.out.println(player.getHealth());
+//        System.out.println(player.getWeapon());
+
+          player.damageByGun1();
+          player.damageByGun1();
+          player.damageByGun2();
+          player.heal();
+
+    }
+
+}
+
